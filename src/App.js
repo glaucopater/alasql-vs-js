8f4178c02032fd989eff816442a0ff23665f408e
Glauco Pater <glauco.pater@gmail.com>
2020-05-28 12:09:02 +0200

    Update style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Alasql VS JavaScript</h1>
-      <h2>Use Chrome for more precision!</h2>
+      <h3>Use Chrome for more precision!</h3>
       <Metrics />
     </div>
   );
